b0c13fcedd4d2133223b2ee8d1edc3d32d9a8ae3
Mohammed Alabdali <ddaf051@gmail.com>
2022-12-29 15:03:43 +0200

    create the card component and render it in the home page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from "react";
+import { Col, Container, Row } from "react-bootstrap";
 import Header from "./components/Header";
+import CardRental from "./components/Card";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   const [rentals, setRentals] = useState([]);
@@ -16,6 +19,19 @@ function App() {
         form={form}
         setRentals={setRentals}
       />
+      <Container>
+        {rentals.length !== 0 ? (
+          <Row className="d-flex justify-content-center">
+            {rentals.data.map((rental) => (
+              <Col key={Math.random()} className="d-flex aling-item-strech">
+                <CardRental rental={rental} setIsShowInfo={setIsShowInfo} />
+              </Col>
+            ))}
+          </Row>
+        ) : (
+          <h1>PLS, Submit the form with your full name & API key</h1>
+        )}
+      </Container>
     </div>
   );
 }
---
+++ src/components/Card.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+
+const CardRental = ({ rental, setIsShowInfo }) => {
+  return (
+    <Card style={{ width: "18rem" }}>
+      <Card.Img
+        variant="top"
+        src={`https://app.hosthub.com${rental.image_path}`}
+      />
+      <Card.Body>
+        <Card.Title>{rental.name}</Card.Title>
+        <div className="py-3">
+          <div className="d-flex justify-content-between">
+            <div className="mx-2">check in time:</div>
+            <div className="mx-2">{rental.check_in_time}</div>
+          </div>
+          <div className="d-flex justify-content-between">
+            <div className="mx-2">checkout time:</div>
+            <div className="mx-2">{rental.checkout_time}</div>
+          </div>
+          <div className="d-flex justify-content-between">
+            <div className="mx-2">active:</div>
+            <div className="mx-2">{rental.status}</div>
+          </div>
+        </div>
+        <Button
+          className="mt-auto"
+          variant="primary"
+          onClick={() => setIsShowInfo(true)}
+          type='button"'
+        >
+          Rental Information
+        </Button>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default CardRental;
